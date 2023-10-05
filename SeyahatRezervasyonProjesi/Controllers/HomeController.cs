3e7c2f947c693c3ae72e386884a301cc7359e391
omer.carnacar <omer.carnacar@DMN124>
2023-10-05 14:58:06 +0300

    Add project files.

---
+++ SeyahatRezervasyonProjesi/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SeyahatRezervasyonProjesi.Models;
+using System.Diagnostics;
+
+namespace SeyahatRezervasyonProjesi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Reception()
+        {
+            return PartialView();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
